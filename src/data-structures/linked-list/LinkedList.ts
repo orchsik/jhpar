fc5d8e98a9f67ab4c44448a7b510edde91a55b14
orchsik <orchsik@gmail.com>
2020-12-18 22:30:27 +0900

    Add 링크드리스트 완성><

--- src/data-structures/linked-list/LinkedList.ts
+++ src/data-structures/linked-list/LinkedList.ts
@@ -1,38 +1,153 @@
-import Comparator from '../comparator/Comparator';
-import LinkedListNode from './LinkedListNode';
+import { defaultEquals, IEqualsFunction } from '../utis';
+import { Node } from '../models/linked-list-models';
 
-export default class LinkedList {
-  head: LinkedListNode = new LinkedListNode();
-  tail: LinkedListNode = new LinkedListNode();
-  compare: Comparator;
-  constructor(comparatorFunction?: Function) {
-    this.compare = new Comparator(comparatorFunction);
-  }
+export default class LinkedList<T> {
+  protected count = 0;
+  protected head: Node<T> | undefined;
 
-  prepend(value: any) {
-    const newNode = new LinkedListNode(value, this.head);
-    this.head = newNode;
-    if (!this.tail) {
-      this.tail = newNode;
-    }
-    return this;
-  }
+  constructor(protected equalsFn: IEqualsFunction<T> = defaultEquals) {}
 
-  append(value: any) {
-    const newNode = new LinkedListNode(value);
+  // next를 가지지 않는 곳 까지~
+  push(element: T) {
+    const newNode = new Node(element);
+    let node;
+
     if (!this.head) {
-      this.head = value;
-      this.tail = value;
-      return this;
+      // console.log('empty linekdList');
+      this.head = newNode;
+    } else {
+      // console.log('linekdList', this);
+      node = this.head;
+      while (node?.next) {
+        node = node.next;
+      }
+      node.next = newNode;
     }
-    this.tail.next = newNode;
-    this.tail = newNode;
-    return this;
+    this.count++;
+  }
+
+  getElementAt(index: number) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head;
+      for (let i = 0; i < index && node; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  insert(element: T, index: number) {
+    if (index >= 0 && index < this.count) {
+      const newNode = new Node(element);
+      if (index === 0) {
+        const node = this.head;
+        newNode.next = node;
+        this.head = newNode;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        newNode.next = previous.next;
+        previous.next = newNode;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  removeAt(index: number) {
+    if (index >= 0 && index < this.count) {
+      let target = this.head;
+      if (index === 0) {
+        this.head = target.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        target = previous.next;
+        previous.next = target.next;
+      }
+
+      this.count--;
+      return target.element; //지워진 원소
+    }
+    return undefined;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  indexOf(element: T) {
+    let node = this.head;
+    for (let i = 0; i < this.size() && node; i++) {
+      if (this.equalsFn(element, node.element)) {
+        return i;
+      }
+      node = node.next;
+    }
+    return -1;
+  }
+
+  remove(element: T) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  clear() {
+    this.head = undefined;
+    this.count = 0;
+  }
+
+  toString() {
+    if (!this.head) {
+      return '';
+    }
+    let objString = `${this.head.element}`;
+    let node = this.head.next;
+    for (let i = 1; i < this.size() && node; i++) {
+      objString = `${objString}, ${node.element}`;
+      node = node.next;
+    }
+    return objString;
   }
 }
 
-const a = new LinkedList();
-a.append(1);
-// a.append(2);
-// a.append(3);
-console.log(a);
+const linkedList = new LinkedList();
+// # push
+linkedList.push(1);
+linkedList.push(2);
+linkedList.push(4);
+linkedList.push(600);
+console.log(linkedList);
+
+// # getElement
+// var ele = linkedList.getElementAt(0);
+// console.log(ele.element);
+// var ele = linkedList.getElementAt(1);
+// console.log(ele.element);
+
+// insert
+// linkedList.insert(3, 2);
+// var ele = linkedList.getElementAt(2);
+// console.log(ele.element);
+
+// removeAt
+// console.log(linkedList.removeAt(3));
+
+// indexOf
+// console.log(linkedList.indexOf(777));
+// console.log(linkedList.indexOf(4));
+// console.log(linkedList.indexOf(2));
+
+// remove
+// console.log(linkedList.remove(600));
+
+// toString
+// console.log(linkedList.toString());
