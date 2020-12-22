1a92a1f1c6041bd834599e62eeac606c83825f01
orchsik <orchsik@gmail.com>
2020-12-22 12:36:30 +0900

    Add: 참고사항

--- src/algorithms/divide-and-conquer/find-biggest-square.ts
+++ src/algorithms/divide-and-conquer/find-biggest-square.ts
@@ -37,3 +37,10 @@ const solution = (farm: Ifarm): ISolution => {
 
 const farm = { width: 1680, height: 640 };
 console.log(solution(farm));
+
+/** 유클리드 호제법 https://ilc12345-080.tistory.com/m/32
+임의의 두 자연수 a, b가 주어졌을때. 둘중 큰 값이 a라고 가정해보겠습니다.
+[#1] a를 b로 나눈 나머지를 n 이라고 하면 (a%b = n)
+[#2] n이 0일때, b가 최대 공약수(GCD)입니다.
+[#3] 만약 n이 0이 아니라면, a에 b값을 다시 넣고 n를 b에 대입 한 후 다시 위에 step2부터 반복하면 됩니다.
+ */
