d0ba1585588e5f292aaa4043f7798ab182b71bde
orchsik <orchsik@gmail.com>
2020-12-23 15:11:16 +0900

    Debug 이진탐색 binaryIdx 설정 부분

--- src/algorithms/divide-and-conquer/binary-search.ts
+++ src/algorithms/divide-and-conquer/binary-search.ts
@@ -5,7 +5,9 @@
  */
 type Isolution = (() => any) | number;
 export const solution = (anyArr: any[], binaryIdx: number, target: any): Isolution => {
-  if (anyArr.length === 1 && anyArr[binaryIdx] !== target) {
+  if (binaryIdx < 0 || binaryIdx > anyArr.length - 1) {
+    return -1;
+  } else if (anyArr.length === 1 && anyArr[binaryIdx] !== target) {
     return -1;
   } else if (anyArr.length === 1 && anyArr[binaryIdx] === target) {
     return binaryIdx;
@@ -15,14 +17,29 @@ export const solution = (anyArr: any[], binaryIdx: number, target: any): Isoluti
   if (binaryValue === target) {
     return binaryIdx;
   } else if (binaryValue > target) {
-    return solution(anyArr, Math.floor(binaryIdx / 2), target);
+    let newBinaryIdx = Math.floor((binaryIdx + 1) / 2);
+    if (newBinaryIdx === binaryIdx) newBinaryIdx = binaryIdx - 1;
+    return solution(anyArr, newBinaryIdx, target);
   } else {
-    return solution(anyArr, Math.floor((binaryIdx + anyArr.length - 1) / 2), target);
+    let newBinaryIdx = Math.floor((binaryIdx + anyArr.length - 1) / 2);
+    if (newBinaryIdx === binaryIdx) newBinaryIdx = binaryIdx + 1;
+    return solution(anyArr, newBinaryIdx, target);
   }
 };
 
 // const anyArr = [1];
+// const anyArr = [1, 2];
+// const anyArr = [1, 2, 3];
+// const anyArr = [1, 2, 3, 4];
 // const anyArr = [1, 2, 3, 4, 5];
 const anyArr = [1, 2, 3, 4, 5, 6];
-const binaryIdx = Math.floor(anyArr.length - 1 / 2);
+const binaryIdx = Math.ceil((anyArr.length - 1) / 2) || 0;
+// console.log({ binaryIdx });
+console.log(solution(anyArr, binaryIdx, 0));
 console.log(solution(anyArr, binaryIdx, 1));
+console.log(solution(anyArr, binaryIdx, 2));
+console.log(solution(anyArr, binaryIdx, 3));
+console.log(solution(anyArr, binaryIdx, 4));
+console.log(solution(anyArr, binaryIdx, 5));
+console.log(solution(anyArr, binaryIdx, 6));
+console.log(solution(anyArr, binaryIdx, 7));
