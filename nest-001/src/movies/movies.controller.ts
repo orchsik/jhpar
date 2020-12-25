055211968d25ed5c79625ceb736762cc28851d7a
orchsik <orchsik@gmail.com>
2020-12-25 17:51:40 +0900

    Add movies Controller

--- nest-001/src/movies/movies.controller.ts
+++ nest-001/src/movies/movies.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
-    return 'this is movies';
+    return 'this will reeturn all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `this will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie.';
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}.`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will update movie with the id: ${movieId}`;
   }
 }
