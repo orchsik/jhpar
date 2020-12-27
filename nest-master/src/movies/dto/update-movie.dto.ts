827502b26883e04272a43cfdf0d4cd6af3c42e5e
orchsik <orchsik@gmail.com>
2020-12-27 23:59:22 +0900

    Fix - 폴더명

--- nest-master/src/movies/dto/update-movie.dto.ts
+++
@@ -1,5 +0,0 @@
-import { IsString, IsNumber } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMovieDto } from './create-movie.dto';
-
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
