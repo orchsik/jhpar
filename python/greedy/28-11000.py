4f45e7b64f1c0ae1a39d0e008ee4d905bb2fdf48
orchsik <orchsik@gmail.com>
2021-01-05 23:53:57 +0900

    SpeedUp Greedy 11000

--- python/greedy/28-11000.py
+++ python/greedy/28-11000.py
@@ -41,41 +41,77 @@
 #     // 새 강의실 개수 추가
 #     roomCnt++;
 #   }
-
 #   return roomCnt;
 # };
-N = int(input())
-lectureTimes = []
-for _ in range(N):
-  s, e = list(map(int, input().split()))
-  lectureTimes.append([s, e])
 
-roomCnt = 0
-processedLectureTimes = []
-while len(lectureTimes) != 0:
-  newRoomTimes = []
-  newLectureTimes = lectureTimes
-  for lectureTimeInfo in lectureTimes:
-    ls, le = lectureTimeInfo
-    isOverlap = False if len(newRoomTimes) == 0 else True
-    for roomTimeInfo in newRoomTimes:
-      rs, re = roomTimeInfo
-      if re > ls & rs < le: pass
-      if rs < le & re > ls: pass
-      else: isOverlap = False
+#
+#
+# 풀이1 시간초과
+# N = int(input())
+# lectureTimes = []
+# for _ in range(N):
+#   s, e = list(map(int, input().split()))
+#   lectureTimes.append([s, e])
+
+# roomCnt = 0
+# processedLectureTimes = []
+# while len(lectureTimes) != 0:
+#   newRoomTimes = []
+#   newLectureTimes = lectureTimes
+#   for lectureTimeInfo in lectureTimes:
+#     ls, le = lectureTimeInfo
+#     isOverlap = False if len(newRoomTimes) == 0 else True
+#     for roomTimeInfo in newRoomTimes:
+#       rs, re = roomTimeInfo
+#       if re > ls & rs < le: pass
+#       if rs < le & re > ls: pass
+#       else: isOverlap = False
     
-    if isOverlap == False:
-      newRoomTimes.append(lectureTimeInfo)
-      processedLectureTimes.append(lectureTimeInfo)
-      newLectureTimes.remove(lectureTimeInfo)
+#     if isOverlap == False:
+#       newRoomTimes.append(lectureTimeInfo)
+#       processedLectureTimes.append(lectureTimeInfo)
+#       newLectureTimes.remove(lectureTimeInfo)
 
-  lectureTimes = newLectureTimes
-  roomCnt += 1
+#   lectureTimes = newLectureTimes
+#   roomCnt += 1
 
-print(roomCnt)
+# print(roomCnt)
   
 
 
+#
+#
+# 풀이2
+# 반복문을 돌면서 처리한 강의를 빼주지않고 우선순위큐로 바로 처리해주자
+# 1 주어진 강의시간을 빨리 시작하는걸로 정렬한다.
+# 2-1 새 강의는 일단 넣어줘
+# 2-2 진행중인 강의가 있는 경우, 가장 빨리 끝나는 강의 시간 <= 새강의시작시간 이면 그 강의실은 새 강의만 진행하게 이전 강의는 빼줘
+import heapq
+import sys
+
+N = int(input())
+lectureTimes = []
+for _ in range(N):
+  # lectureTimes.append(list(map(int, input().split())))
+  # for performance
+  lectureTimes.append(list(map(int, sys.stdin.readline().split())))
+
+# 강의 시작 시간 오름차순 정렬
+# lectureTimes = sorted(lectureTimes, key = lambda timeinfo: timeinfo[0])
+lectureTimes.sort(key=lambda timeinfo: timeinfo[0])
+
+usedRooms = []
+for timeinfo in lectureTimes:
+  startTime, endTime = timeinfo
+  if len(usedRooms) != 0 and usedRooms[0] <= startTime:
+    heapq.heappop(usedRooms)
+  heapq.heappush(usedRooms, endTime)
+
+print(len(usedRooms))
 
 
-        
+
+
+
+
+
