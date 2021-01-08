1e3f2d76df62c66c525c9f3257dca6efbbab99e0
orchsik <orchsik@gmail.com>
2021-01-08 16:44:43 +0900

    Add Greedy 1931

--- fundmental/src/algorithms/greedy/1931.ts
+++ fundmental/src/algorithms/greedy/1931.ts
@@ -1,51 +1,78 @@
-export const solution = (input: number[][]) => {
-  let answer = 0;
+// 시간초과
+// export const solution = (input: number[][]) => {
+//   let answer = 0;
 
-  // 시간시간 정렬
+//   // 시간시간 정렬
+//   input.sort((item_1, item_2) => {
+//     return item_1[0] - item_2[0];
+//   });
+
+//   // 시작시간이 같은 경우 종료시간이 작은 회의만 남겨
+//   let _input: number[][] = [];
+//   for (let i = 0; i < input.length; i++) {
+//     if (!input[i + 1]) {
+//       _input.push(input[i]);
+//       continue;
+//     }
+
+//     const [a_start, a_end] = input[i];
+//     const [b_start, b_end] = input[i + 1];
+//     if (a_start === b_start) {
+//       if (b_end > a_end) {
+//         _input.push(input[i]);
+//       } else {
+//         _input.push(input[i + 1]);
+//       }
+//       i++;
+//     } else {
+//       _input.push(input[i]);
+//     }
+//   }
+
+//   // 가장 많이 배정할 수 있는 경우 찾기
+//   console.log({ _input });
+//   let maxCnt = 0;
+//   for (let i = 0; i < _input.length; i++) {
+//     const pivots = _input.slice(i);
+//     let tmp: number[] = [pivots[0][1]];
+//     while (pivots.length !== 0) {
+//       const [start, end] = pivots.shift();
+//       if (tmp[tmp.length - 1] <= start) {
+//         tmp.push(end);
+//       }
+//     }
+
+//     if (maxCnt < tmp.length) {
+//       maxCnt = tmp.length;
+//     }
+//   }
+
+//   console.log(maxCnt);
+//   return 4;
+// };
+
+//
+//
+//
+// 빠른 풀이
+export const solution = (input: number[][]) => {
+  // 종료시간 오름차순 + 종료시간 같은 경우 시작시간 오름차순
   input.sort((item_1, item_2) => {
     return item_1[0] - item_2[0];
   });
+  input.sort((item_1, item_2) => {
+    return item_1[1] - item_2[1];
+  });
 
-  // 시작시간이 같은 경우 종료시간이 작은 회의만 남겨
-  let _input: number[][] = [];
-  for (let i = 0; i < input.length; i++) {
-    if (!input[i + 1]) {
-      _input.push(input[i]);
-      continue;
-    }
-
-    const [a_start, a_end] = input[i];
-    const [b_start, b_end] = input[i + 1];
-    if (a_start === b_start) {
-      if (b_end > a_end) {
-        _input.push(input[i]);
-      } else {
-        _input.push(input[i + 1]);
-      }
-      i++;
-    } else {
-      _input.push(input[i]);
+  // 최대 회의개수 그리디 시작
+  let nowEndTime = 0;
+  let meetingCnt = 0;
+  for (const meeting of input) {
+    if (nowEndTime <= meeting[0]) {
+      meetingCnt++;
+      nowEndTime = meeting[1];
     }
   }
 
-  // 가장 많이 배정할 수 있는 경우 찾기
-  console.log({ _input });
-  let maxCnt = 0;
-  for (let i = 0; i < _input.length; i++) {
-    const pivots = _input.slice(i);
-    let tmp: number[] = [pivots[0][1]];
-    while (pivots.length !== 0) {
-      const [start, end] = pivots.shift();
-      if (tmp[tmp.length - 1] <= start) {
-        tmp.push(end);
-      }
-    }
-
-    if (maxCnt < tmp.length) {
-      maxCnt = tmp.length;
-    }
-  }
-
-  console.log(maxCnt);
-  return 4;
+  return meetingCnt;
 };
--- python/greedy/28-1931.py
+++ python/greedy/28-1931.py
@@ -3,6 +3,7 @@
 #       [3, 5],
 #       [0, 6],
 #       [5, 7],
+#       [6, 7],
 #       [3, 8],
 #       [5, 9],
 #       [6, 10],
@@ -16,51 +17,81 @@
 #
 #
 # 시간초과!!!!!!!!
-import heapq
+# import heapq
+# import sys
+
+# N = int(input())
+# inputList = []
+# for _ in range(N):
+#   inputList.append(list(map(int, sys.stdin.readline().split())))
+
+# # 1 시작시간 빠른순 정렬
+# meetinglist = []
+# for d in inputList:
+#   heapq.heappush(meetinglist, (d[0], d[1]))
+
+# # 2 시작시간이 같은 경우 종료시간이 작은 회의만 남겨
+# mlist = []
+# while meetinglist:
+#   item = heapq.heappop(meetinglist)
+
+#   if len(mlist) == 0:
+#     mlist.append(item)
+#     continue
+#   beforeStart, beforeEnd = mlist[len(mlist) - 1]
+
+#   start, end = item
+#   if beforeStart == start and beforeEnd > end:
+#     mlist.pop()
+#     mlist.append(item)
+#   else: mlist.append(item)
+
+# # 3 가장 많이 배정할 수 있는 경우 찾기
+# maxCnt = 0
+# for i in range(len(mlist)):
+#   pivots = mlist[i:]
+#   if maxCnt > len(pivots): break
+
+#   tmp = [pivots[0][1]]
+#   while pivots:
+#     start, end = heapq.heappop(pivots)
+#     if tmp[len(tmp) -1] <= start:
+#       tmp.append(end)
+
+#   if maxCnt < len(tmp):
+#     maxCnt = len(tmp)
+
+# print(maxCnt)
+
+
+#
+#
+# 빠른 풀이
+# 빨리 끝나는거로만 모으면 하나의 회의실에서 최대한 많은 회의를 할 수 있을거 같다.
+# 2-2 / 1-2 끝나는 시간이 같은 경우, 저렇게 정렬되면 회의 하나만 할 수 있어.. 시작시간 빠른거 먼저 정렬하자
 import sys
 
 N = int(input())
-inputList = []
+meetings = []
 for _ in range(N):
-  inputList.append(list(map(int, sys.stdin.readline().split())))
+  meetings.append(list(map(int, sys.stdin.readline().split())))
 
-# 1 시작시간 빠른순 정렬
-meetinglist = []
-for d in inputList:
-  heapq.heappush(meetinglist, (d[0], d[1]))
+# 1 빨리 끝나는 순서대로 정렬 + 끝나는 순서가 같으면 시작 시간이 빠른거로 정렬
+meetings.sort(key=lambda item: item[0])
+meetings.sort(key=lambda item: item[1])
 
-# 2 시작시간이 같은 경우 종료시간이 작은 회의만 남겨
-mlist = []
-while meetinglist:
-  item = heapq.heappop(meetinglist)
+# 2 최대 회의개수 그리디 시작
+nowEndTime = 0
+meetingCnt = 0
+for meeting in meetings:
+  if meeting[0] >= nowEndTime:
+    nowEndTime = meeting[1]
+    meetingCnt += 1
 
-  if len(mlist) == 0:
-    mlist.append(item)
-    continue
-  beforeStart, beforeEnd = mlist[len(mlist) - 1]
+print(meetingCnt)
 
-  start, end = item
-  if beforeStart == start and beforeEnd > end:
-    mlist.pop()
-    mlist.append(item)
-  else: mlist.append(item)
 
-# 3 가장 많이 배정할 수 있는 경우 찾기
-maxCnt = 0
-for i in range(len(mlist)):
-  pivots = mlist[i:]
-  if maxCnt > len(pivots): break
 
-  tmp = [pivots[0][1]]
-  while pivots:
-    start, end = heapq.heappop(pivots)
-    if tmp[len(tmp) -1] <= start:
-      tmp.append(end)
-
-  if maxCnt < len(tmp):
-    maxCnt = len(tmp)
-
-print(maxCnt)
 
 
 
