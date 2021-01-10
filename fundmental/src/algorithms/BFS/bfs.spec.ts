60bf55279b9e7ba3a511d49feba473b920dfb5aa
orchsik <orchsik@gmail.com>
2021-01-10 21:08:38 +0900

    Add: 14502 풀이 준비

---
+++ fundmental/src/algorithms/BFS/bfs.spec.ts
@@ -0,0 +1,41 @@
+import { solution } from './14502';
+
+describe('', () => {
+  it('test1', () => {
+    // var inputs = [-1, 2, 1, 3];
+    var answer = solution();
+    expect(answer).toEqual(6);
+  });
+
+  // it('test2', () => {
+  //   var inputs = [
+  //     [3, 6],
+  //     [7, 3],
+  //     [4, 2],
+  //     [1, 4],
+  //     [5, 7],
+  //     [2, 5],
+  //     [6, 1],
+  //   ];
+  //   var answer = solution(inputs);
+  //   expect(answer).toEqual(3);
+  // });
+
+  // it('test3', () => {
+  //   var input = 6;
+  //   var answer = solution(input);
+  //   expect(answer).toEqual(2);
+  // });
+
+  // it('test4', () => {
+  //   var input = 9;
+  //   var answer = solution(input);
+  //   expect(answer).toEqual(3);
+  // });
+
+  // it('test5', () => {
+  //   var input = 11;
+  //   var answer = solution(input);
+  //   expect(answer).toEqual(3);
+  // });
+});
